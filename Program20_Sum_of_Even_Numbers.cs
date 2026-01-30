421717ed83b9661558041d557b0222a4aa685c93
Haris Momin <harismomin45@gmail.com>
2026-01-30 16:27:29 +0530

    Add Program to sum even numbers from 2 to 50

---
+++ Program20_Sum_of_Even_Numbers.cs
@@ -0,0 +1,9 @@
+using System;
+class Program {
+    static void Main() {
+        int sum = 0;
+        for (int i = 2; i <= 50; i += 2)
+            sum += i;
+        Console.WriteLine(sum);
+    }
+}
